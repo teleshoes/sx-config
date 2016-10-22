32a177f8aafbe219d0a0c345db66eb8c1d0d3f20
Elliot Wolk <elliot.wolk@gmail.com>
2016-10-22 12:00:32 -0400

    sms-reformat-sort: add script to sort texts

---
+++ comm-tools/sms-reformat-sort.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+sub main(@){
+  for my $file(`ls *.sms`){
+    chomp $file;
+    next if -l $file;
+    my %lines;
+    open FH, "< $file" or die "could not read $file\n$!\n";
+    while(my $line = <FH>){
+      if($line !~ /^([0-9+]+),(\d+),(\d+),(S|M),(INC|OUT),(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d),"(.*)"$/i){
+        die "malformed line: $line";
+      }
+      my ($num, $dateMillis, $dateSentMillis, $type, $dir, $dateFmt, $msg) = ($1, $2, $3, $4, $5, $6, $7);
+
+      #$type = "";
+
+      #$dir = $dir eq "OUT" ? 1 : 2;
+
+      #$dateSentMillis = 0;
+
+      #$dateMillis = sprintf "%020d", $dateMillis;
+      #$dateSentMillis = sprintf "%020d", $dateMillis;
+
+      my $sortKey = "$num-$dateMillis-$dateSentMillis-$type-$dir-$msg";
+      die "DUPE LINE: $line" if defined $lines{$sortKey};
+      $lines{$sortKey} = $line;
+    }
+    close FH;
+    open FH, "> $file";
+    for my $sortKey(sort keys %lines){
+      print FH $lines{$sortKey};
+    }
+    close FH;
+  }
+}
+
+&main(@ARGV);
