0804c687042b6c0c9ea2c98ec725e488dbbfca1b
Elliot Wolk <elliot.wolk@gmail.com>
2016-12-03 12:31:44 -0500

    sqlite-dump: include 'PRAGMA user_version=VALUE' in dump

--- comm-tools/sqlite-dump.pl
+++ comm-tools/sqlite-dump.pl
@@ -2,9 +2,12 @@
 use strict;
 use warnings;
 
+my @pragmas = qw(user_version);
+
 my $usage = "Usage:
   $0 SQLITE_DB OUTPUT_TXT_FILE
     dump SQLITE_DB to text with \"sqlite3 SQLITE_DB .dump\"
+    additionally, generate setters for the following pragmas: [@pragmas]
     write output to OUTPUT_TXT_FILE
 ";
 
@@ -21,6 +24,18 @@ sub main(@){
   @lines = (@lines, @dumpLines);
   close FH;
   die "error running sqlite3\n" if $? != 0;
+
+  for my $pragma(@pragmas){
+    open FH, "-|", "sqlite3", $db, "pragma $pragma"
+      or die "could not run sqlite3\n$!\n";
+    my @pragmaLines = <FH>;
+    die "malformed .pragma $pragma output\n" if @pragmaLines != 1;
+    my $val = $pragmaLines[0];
+    chomp $val;
+    push @lines, "PRAGMA $pragma=$val;\n";
+    close FH;
+    die "error running sqlite3\n" if $? != 0;
+  }
 
   open FH, "> $outFile" or die "could not write $outFile\n$!\n";
   print FH @lines;
