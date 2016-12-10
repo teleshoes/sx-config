12418dc9f08926f8e4f94cd05cfd5cb1ea5c9c67
Elliot Wolk <elliot.wolk@gmail.com>
2016-12-10 13:03:08 -0500

    sms_db_importer: upgrade from github.com/teleshoes/Android-SMS-DB-importer

--- comm-tools/sms_db_importer.py
+++ comm-tools/sms_db_importer.py
@@ -359,6 +359,8 @@ class MMSPart:
     self.body = None
 
 def cleanNumber(number):
+  if number == None:
+    number = ''
   number = re.sub(r'[^+0-9]', '', number)
   number = re.sub(r'^\+?1(\d{10})$', '\\1', number)
   return number
@@ -540,8 +542,12 @@ def readMMSFromAndroid(db_file, mms_parts_dir):
     filepath = row[3]
     body = row[4]
 
+    #skip malformed SMIL, happens for NTF messages
+    if msg_id not in msgs and part_type == "application/smil":
+      continue
+
     if msg_id not in msgs:
-      print "INVALID MESSAGE ID FOR ADDRESS: " + str(row)
+      print "INVALID MESSAGE ID FOR MMS PART: " + str(row)
       quit(1)
     msg = msgs[msg_id]
 
