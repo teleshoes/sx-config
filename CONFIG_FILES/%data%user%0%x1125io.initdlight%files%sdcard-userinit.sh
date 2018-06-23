b51131a327bcd59b46d4a80b4c0f3f3bc6d2de96
Elliot Wolk <elliot.wolk@gmail.com>
2018-06-23 16:26:48 -0400

    config-files: delete obsolete/n9-specific/broken config files

--- CONFIG_FILES/%data%user%0%x1125io.initdlight%files%sdcard-userinit.sh
+++
@@ -1,9 +0,0 @@
-DIR=/data/user/0/x1125io.initdlight
-LOGOUT=$DIR/out
-LOGERR=$DIR/err
-
-rm -f $LOGOUT $LOGERR
-
-sh /sdcard/userinit/userinit-adb-over-network.sh >>$LOGOUT 2>>$LOGERR
-sh /sdcard/userinit/userinit-linuxdeploy.sh >>$LOGOUT 2>>$LOGERR
-sh /sdcard/userinit/userinit-mount.sh >>$LOGOUT 2>>$LOGERR
