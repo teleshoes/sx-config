a90543690174d4425534ad19c228d0d240eecce4
Elliot Wolk <elliot.wolk@gmail.com>
2018-06-24 12:00:56 -0400

    sailfish: delete all remaining android+meego s5/n9/n900 scripts and tools

--- userinit/userinit-adb-over-network.sh
+++
@@ -1,3 +0,0 @@
-setprop service.adb.tcp.port 5555
-stop adbd
-start adbd
--- userinit/userinit-linuxdeploy.sh
+++
@@ -1,2 +0,0 @@
-/data/data/ru.meefik.linuxdeploy/bin/linuxdeploy mount
-/data/data/ru.meefik.linuxdeploy/bin/linuxdeploy start
