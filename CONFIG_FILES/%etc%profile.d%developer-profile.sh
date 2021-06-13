53718e5c7d923438958e8d8a5f4054419d8c8e63
Elliot Wolk <elliot.wolk@gmail.com>
2021-06-13 16:13:28 -0400

    config-files: upgrade SFOS 4.0.1.48 => 4.1.0.24
    unchanged  %etc%connman%firewall.conf
    unchanged  %etc%connman%firewall.d%10-block-icmp-firewall.conf
    unchanged  %etc%mce%20hybris-led.ini
    CONFLICTS  %etc%profile.d%developer-profile.sh
    unchanged  %etc%pulse%mainvolume-listening-time-notifier.conf
    unchanged  %etc%sudoers
    merged     %usr%share%jolla-messages%pages%conversation%SMSMessageDelegate.qml
    unchanged  %usr%share%jolla-messages%pages%groups%GroupDelegate.qml
    unchanged  %usr%share%jolla-settings%entries%sounds.json
    unchanged  %usr%share%jolla-settings%pages%sounds%SoundsPage.qml
    unchanged  %usr%share%jolla-settings%pages%text_input%textinput.qml
    unchanged  %usr%share%jolla-settings%pages%wlan%EnableSwitch.qml
    unchanged  %usr%share%lipstick-jolla-home-qt5%eventsview%EventsViewList.qml
    unchanged  %usr%share%lipstick-jolla-home-qt5%notifications%NotificationPreview.qml
    unchanged  %usr%share%lipstick-jolla-home-qt5%switcher%Switcher.qml
    unchanged  %usr%share%lipstick-jolla-home-qt5%topmenu%FavoriteSettingsLoader.qml
    unchanged  %usr%share%lipstick-jolla-home-qt5%topmenu%TopMenu.qml
    unchanged  %usr%share%lipstick-jolla-home-qt5%volumecontrol%VolumeControl.qml
    unchanged  %usr%share%maliit%plugins%com%jolla%InputHandler.qml
    unchanged  %usr%share%maliit%plugins%com%jolla%KeyboardBase.qml
    unchanged  %usr%share%maliit%plugins%eu%cpbm%okboard%CurveKeyboardBasePatch.qml

--- CONFIG_FILES/%etc%profile.d%developer-profile.sh
+++ CONFIG_FILES/%etc%profile.d%developer-profile.sh
@@ -10,131 +10,137 @@ if [ $CURRENT_UID -ne 0 ]; then
 # in either of the lists
 
 # (These must have newline as separator)
-    BLACKLIST_VALUES=$(sed -e 's/^/-e ^/' -e 's/$/=/' <<-EOF
-	AG_PROVIDERS
-	AG_SERVICES
-	AG_SERVICE_TYPES
-	board_id
-	BOOT_IMAGE
-	bootmode
-	bootreason
-	BOOTSTATE
-	bootup_reason
-	crashkernel
-	dbi-size
-	dbi-type
-	dbi-uid
-	dbi-vendor
-	do_fsck
-	emmc_wp_size
-	G_BROKEN_FILENAMES
-	HOME
-	hwid
-	imei
-	LANG
-	LC_COLLATE
-	LOGNAME
-	mem_issw
-	NGF_FFMEMLESS_SETTINGS
-	nolo
-	NOTIFY_SOCKET
-	OLDPWD
-	INVOCATION_ID
-	JOURNAL_STREAM
-	PATH
-	product_model
-	product_name
-	PWD
-	pwr_on_status
-	QT_WAYLAND_COMPOSITOR_NO_THROTTLE
-	QTCONTACTS_MANAGER_OVERRIDE
-	Qunlock
-	serialnumber
-	SESSION_TARGET
-	SHELL
-	SHLVL
-	TERM
-	USER
-	vga
-	vhash
-	wifibin
-	wlanmac
-	XDG_SEAT
-	XDG_SESSION_ID
-	XDG_VTNR
+	BLACKLIST_VALUES=$(sed -e 's/^/-e ^/' -e 's/$/=/' <<-EOF
+		AG_PROVIDERS
+		AG_SERVICES
+		AG_SERVICE_TYPES
+		board_id
+		BOOT_IMAGE
+		bootmode
+		bootreason
+		BOOTSTATE
+		bootup_reason
+		crashkernel
+		dbi-size
+		dbi-type
+		dbi-uid
+		dbi-vendor
+		do_fsck
+		emmc_wp_size
+		G_BROKEN_FILENAMES
+		HOME
+		hwid
+		imei
+		LANG
+		LC_COLLATE
+		LOGNAME
+		mem_issw
+		NGF_FFMEMLESS_SETTINGS
+		nolo
+		NOTIFY_SOCKET
+		OLDPWD
+		INVOCATION_ID
+		JOURNAL_STREAM
+		PATH
+		product_model
+		product_name
+		PWD
+		pwr_on_status
+		QT_WAYLAND_COMPOSITOR_NO_THROTTLE
+		QTCONTACTS_MANAGER_OVERRIDE
+		Qunlock
+		serialnumber
+		SESSION_TARGET
+		SHELL
+		SHLVL
+		TERM
+		USER
+		vga
+		vhash
+		wifibin
+		wlanmac
+		XDG_SEAT
+		XDG_SESSION_ID
+		XDG_VTNR
+		LAST_LOGIN_UID
 	EOF
-)
+	)
 
-    WHITELIST_VALUES=$(sed -e 's/^/-e ^/' -e 's/$/=/' <<-EOF
-	LIPSTICK2VNC_OPTS
-	BROWSER
-	DBUS_SESSION_BUS_ADDRESS
-	DISPLAY
-	EGL_DRIVER
-	EGL_PLATFORM
-	FF_MEMLESS_SETTINGS
-	GSETTINGS_BACKEND
-	HYBRIS_LD_LIBRARY_PATH
-	M_DECORATED
-	MOZ_GMP_PATH
-	OPTIONS
-	QMLSCENE_DEVICE
-	QML_FIXED_ANIMATION_STEP
-	QSG_FIXED_ANIMATION_STEP
-	QT_DEFAULT_RUNTIME_SYSTEM
-	QT_GRAPHICSSYSTEM
-	QT_IM_MODULE
-	QT_QPA_PLATFORM
-	QT_USE_DRAG_DISTANCE
-	QT_WAYLAND_DISABLE_WINDOWDECORATION
-	QT_GSTREAMER_CAMERABIN_SRC
-	QT_GSTREAMER_CAMERABIN_FLAGS
-	QT_GSTREAMER_PLAYBIN_FLAGS
-	QT_MESSAGE_PATTERN
-	QT_DF_BASE
-	QT_DF_BASEDEVIATION
-	QT_DF_SCALEFORMAXDEV
-	QT_DF_SCALEFORNODEV
-	QT_DF_RANGE
-	QT_OPENGL_NO_BGRA
-	QT_WAYLAND_RESIZE_AFTER_SWAP
-	QT_WAYLAND_FORCE_DPI
-	WAYLAND_DISPLAY
-	XDG_RUNTIME_DIR
+	if [ -f /etc/profile.d/developer-profile.local ]; then
+		. /etc/profile.d/developer-profile.local
+		if [ ! -z "$BLACKLIST_VALUES_LOCAL" ]; then
+			BLACKLIST_VALUES="${BLACKLIST_VALUES} \
+				$(echo $BLACKLIST_VALUES_LOCAL \
+				| sed -e 's/ /= -e ^/g' -e 's/^/-e ^/' -e 's/$/=/')"
+		fi
+	fi
+
+	WHITELIST_VALUES=$(sed -e 's/^/-e ^/' -e 's/$/=/' <<-EOF
+		LIPSTICK2VNC_OPTS
+		BROWSER
+		DBUS_SESSION_BUS_ADDRESS
+		DISPLAY
+		EGL_DRIVER
+		EGL_PLATFORM
+		FF_MEMLESS_SETTINGS
+		GSETTINGS_BACKEND
+		HYBRIS_LD_LIBRARY_PATH
+		M_DECORATED
+		MOZ_GMP_PATH
+		OPTIONS
+		QMLSCENE_DEVICE
+		QML_FIXED_ANIMATION_STEP
+		QSG_FIXED_ANIMATION_STEP
+		QT_DEFAULT_RUNTIME_SYSTEM
+		QT_GRAPHICSSYSTEM
+		QT_IM_MODULE
+		QT_QPA_PLATFORM
+		QT_USE_DRAG_DISTANCE
+		QT_WAYLAND_DISABLE_WINDOWDECORATION
+		QT_GSTREAMER_CAMERABIN_SRC
+		QT_GSTREAMER_CAMERABIN_FLAGS
+		QT_GSTREAMER_PLAYBIN_FLAGS
+		QT_MESSAGE_PATTERN
+		QT_DF_BASE
+		QT_DF_BASEDEVIATION
+		QT_DF_SCALEFORMAXDEV
+		QT_DF_SCALEFORNODEV
+		QT_DF_RANGE
+		QT_OPENGL_NO_BGRA
+		QT_WAYLAND_RESIZE_AFTER_SWAP
+		QT_WAYLAND_FORCE_DPI
+		WAYLAND_DISPLAY
+		XDG_RUNTIME_DIR
+		ZYPP_LOGFILE
 	EOF
-)
+	)
 
-    SESSION_PID=""
-    for PID in $(/sbin/pidof systemd)
-    do
-        if [ $CURRENT_UID -eq $(ls -ldn /proc/$PID | tr -s ' ' | cut -d' ' -f3) ]
-        then
-            SESSION_PID=$PID
-            break
-        fi
-    done
+	DBUS_SOCKET="/run/user/$CURRENT_UID/dbus/user_bus_socket"
 
-    if [ -n "$SESSION_PID" ]; then
-        # Pick up env variables that
-        # a) are not blacklisted
-        # b) are whitelisted
-        # FIXME: The FOO='BAR BAR' escaping could be more robust...
-        eval "$(tr '\0' '\n' < /proc/$SESSION_PID/environ |
-        grep -v $BLACKLIST_VALUES |
-        grep $WHITELIST_VALUES |
-        sed -e 's/^/export /' -e 's/=/='\''/' -e 's/$/'\''/')"
-        # Report env variables that
-        # a) are not blacklisted
-        # b) are not whitelisted
-        tr '\0' '\n' < /proc/$SESSION_PID/environ |
-        grep -v $BLACKLIST_VALUES |
-        grep -v $WHITELIST_VALUES |
-        sed -e 's/^/NOTICE: Env value ignored: /'
+	if [ -e "$DBUS_SOCKET" ]; then
+		export DBUS_SESSION_BUS_ADDRESS="unix:path=$DBUS_SOCKET"
+		# Pick up env variables that
+		# a) are not blacklisted
+		# b) are whitelisted
+		eval "$(systemctl --user show-environment |
+		grep -v $BLACKLIST_VALUES |
+		grep $WHITELIST_VALUES |
+		sed -e 's/^/export /')"
+		if [ "$0" != "${0#-}" ]; then
+			# This is a login shell
+			# Report env variables that
+			# a) are not blacklisted
+			# b) are not whitelisted
+			systemctl --user show-environment |
+			grep -v $BLACKLIST_VALUES |
+			grep -v $WHITELIST_VALUES |
+			sed -e 's/^/NOTICE: Env value ignored: /'
+		fi
 
-    else
-        # No session running for this user
-        echo "NOTICE: There is no systemd user session running"
-    fi
+	else
+		# No session running for this user
+		echo "NOTICE: There is no systemd user session running"
+	fi
 fi
 
 # Unset local variables to not pollute the environment
